76d5d0b4c0897dce5dca4254f4bd1c179b96b824
Xinzhe Chai <xinzhe_chai@brown.edu>
2021-01-06 21:52:49 -0500

    completed navbar

---
+++ new/js/index.js
@@ -0,0 +1,11 @@
+window.onload = init;
+
+function init() {
+    // $(".nav-button").on('mouseenter', function () {
+    //     $(".nav-button").addClass("shadow-md");
+    // });
+
+    // $(".nav-button").on('mouseleave', function () {
+    //     $(".nav-button").removeClass("shadow-md");
+	// });
+}
